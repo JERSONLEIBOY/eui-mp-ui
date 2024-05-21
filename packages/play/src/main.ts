626f701d2573348cfb1bc36115b5c72826ea5fe6
linfuliang <1423835668@qq.com>
2024-05-21 17:58:48 +0800

    :tada: second commit

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import EuiMpUi from "eui-mp-ui";
+import "eui-mp-ui/dist/index.css";
 
-createApp(App).use(EuiMpUi).mount('#app')
+const app = createApp(App);
+app.use(EuiMpUi);
+app.mount("#app");
