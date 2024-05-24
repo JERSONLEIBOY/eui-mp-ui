5c4e1fbc4eef8239794cf93d5f3f38412e5df66b
linfuliang <1423835668@qq.com>
2024-05-24 15:51:59 +0800

    :tada: 新增Collapse

---
+++ packages/components/Collapse/Collapse.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="er-collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+const COMPONENT_NAME = "ErCollapse" as const;
+defineOptions({
+  name: COMPONENT_NAME,
+})
+</script>
+
+<style scoped>
+@import "./style.css";
+</style>
