d93f6bf91498f89217f6b7d9db01a4e332903ac8
linfuliang <1423835668@qq.com>
2024-05-27 12:07:23 +0800

    :tada: 新增collapse

--- packages/core/components.ts
+++ packages/core/components.ts
@@ -1,5 +1,5 @@
-import { ErButton, ErIcon } from "@eui-mp-ui/components";
+import { ErButton, ErIcon, ErCollapse, ErCollapseItem } from "@eui-mp-ui/components";
 
 import type { Plugin } from "vue";
 
-export default [ErButton, ErIcon] as Plugin[];
+export default [ErButton, ErIcon, ErCollapse, ErCollapseItem] as Plugin[];
--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -4,6 +4,4 @@ import App from './App.vue'
 import EuiMpUi from "eui-mp-ui";
 import "eui-mp-ui/dist/index.css";
 
-const app = createApp(App);
-app.use(EuiMpUi);
-app.mount("#app");
+createApp(App).use(EuiMpUi).mount('#app')
