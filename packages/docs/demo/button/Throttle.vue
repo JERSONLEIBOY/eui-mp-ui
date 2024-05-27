33b1e64f1dcba99143be999aec2d5d5162e9d217
linfuliang <1423835668@qq.com>
2024-05-27 16:04:47 +0800

    :tada: 新增button组件文档

---
+++ packages/docs/demo/button/Throttle.vue
@@ -0,0 +1,14 @@
+<script setup>
+function handleBtnClick() {
+  alert("btn click");
+}
+</script>
+
+<template>
+  <p>
+    <er-button @click="handleBtnClick"> with throttle</er-button>
+    <er-button :use-throttle="false" @click="handleBtnClick"
+      >without throttle</er-button
+    >
+  </p>
+</template>
