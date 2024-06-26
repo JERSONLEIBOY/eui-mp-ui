6876cc7f564d981b2c0f7e22b4cacc353c13481c
linfuliang <1423835668@qq.com>
2024-06-26 17:03:43 +0800

    feat: 新增内容

--- packages/core/components.ts
+++ packages/core/components.ts
@@ -1,5 +1,5 @@
-import { ErButton, ErIcon, ErCollapse, ErCollapseItem, ErAlert } from "@eui-mp-ui/components";
+import { ErButton, ErIcon, ErCollapse, ErCollapseItem, ErAlert, ErTooltip, } from "@eui-mp-ui/components";
 
 import type { Plugin } from "vue";
 
-export default [ErButton, ErIcon, ErCollapse, ErCollapseItem, ErAlert] as Plugin[];
+export default [ErButton, ErIcon, ErCollapse, ErCollapseItem, ErAlert, ErTooltip] as Plugin[];
